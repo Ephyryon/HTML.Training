4b34ca9b84ba01a4cb78b55115214aa82db75d67
JonatanVG <jonatangr@bfk.no>
2024-09-05 08:57:40 +0200

    Update.3.5.2.Comments

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 var coll = document.getElementsByClassName("collapsible");
 var i;
-
+/*These variables and the code bellow allows the collapsible to function by activating different classes based of whether the collapsible is activated or not.*/
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
